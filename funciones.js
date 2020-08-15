5e62c3344056dc05ad69a48175cf88cd1f76b961
DeadSilence0826 <bemarin54@misena.edu.co>
2020-08-15 12:21:37 -0500

    Delete funciones.js

--- funciones.js
+++
@@ -1,6 +0,0 @@
-function ver(){
-    let name = prompt("inserte su nombre");
-    let lastname = prompt("insertre su apellido");
-    document.write ("Su nombre es" + name);
-    document.write ("Su apellido es" + lastname);
-}

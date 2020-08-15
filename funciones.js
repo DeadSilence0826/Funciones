c2a025062a82d60d30f80de4995a0eef33a498cb
Juanbloxx201899 <69650676+Juanbloxx201899@users.noreply.github.com>
2020-08-15 12:21:07 -0500

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,6 @@
+function ver(){
+    let name = prompt("inserte su nombre");
+    let lastname = prompt("insertre su apellido");
+    document.write ("Su nombre es" + name);
+    document.write ("Su apellido es" + lastname);
+}
